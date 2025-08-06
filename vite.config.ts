c01ae4642908bdd22c672a88fafcfa397b152bfc
sxuce <vikram4king@googlemail.com>
2025-08-06 22:44:31 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Use relative paths for assets
+  // This is the repository name on GitHub.
+  // It tells Vite to prepend /gastech-website/ to all asset paths during build.
+  base: '/gastech-website/',
 })
