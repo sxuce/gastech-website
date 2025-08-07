d8fbdd7ebeebf14f19425c1b7e5a2a72259e3760
sxuce <vikram4king@googlemail.com>
2025-08-07 17:11:37 +0100

    seo

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // This is the repository name on GitHub.
-  // It tells Vite to prepend /gastech-website/ to all asset paths during build.
-  base: '/gastech-website/',
+  base: '/',
 })
